42414c4634db5717e7125e8ac168080529c59ca0
cw1 <cw1@users.noreply.github.com>
2015-01-23 17:47:17 +0100

    Update startup.sh
    added interpretation of variable FIRST_RUN
    added generation of owncloud.conf for apache through template

--- startup.sh
+++ startup.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Correct permissions on volumes
-chown -R apache /var/www/html/owncloud/data
-chown -R apache /var/www/html/owncloud/config
+if [ -z "FIRST_RUN" ]; then
+  echo "Container already configured."
+else
+  # Correct permissions on volumes
+  chown -R apache /var/www/html/owncloud/data
+  chown -R apache /var/www/html/owncloud/config
+  
+  # generating owncloud.conf for apache through template
+  if [ -z "$OWNCLOUD_URL_PATH" ]; then
+    OWNCLOUD_ALIAS=""
+  else
+    OWNCLOUD_ALIAS="<IfModule mod_alias.c> Alias /$OWNCLOUD_URL_PATH /var/www/html/owncloud </IfModule>"
+  sed -e "s#\$OWNCLOUD_ALIAS#$OWNCLOUD_ALIAS#" < /template/owncloud.conf >> /etc/httpd/conf.d/owncloud.conf
+fi
 
 /usr/sbin/httpd -DFOREGROUND -k start
