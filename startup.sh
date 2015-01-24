4014777b5be45a49b947e2e7aa5627a785022142
cw1 <cw1@users.noreply.github.com>
2015-01-24 12:26:59 +0100

    added echo to be more verbose

--- startup.sh
+++ startup.sh
@@ -3,22 +3,25 @@
 if [ -z "FIRST_RUN" ]; then
   echo "Container already configured."
 else
-  # Correct permissions on volumes
+  echo "Setting up container."
+  
+  echo "Correcting permissions on volumes."
   chown -R apache /var/www/html/owncloud/data
   chown -R apache /var/www/html/owncloud/config
   
-  # generating owncloud.conf for apache through template
+  echo "Generating owncloud.conf for apache through template."
   if [ -z "$OWNCLOUD_URL_PATH" ]; then
     OWNCLOUD_ALIAS=""
   else
     OWNCLOUD_ALIAS="<IfModule mod_alias.c> Alias /$OWNCLOUD_URL_PATH /var/www/html/owncloud </IfModule>"
   sed -e "s#\$OWNCLOUD_ALIAS#$OWNCLOUD_ALIAS#" < /template/owncloud.conf >> /etc/httpd/conf.d/owncloud.conf
   
-  # generating autoconfig.php db part through template
+  echo "Generating autoconfig.php db part through template."
   if [ -z "$DB_TYPE" ]; then
-    DB_TYPE=sqlite
+    DB_TYPE="sqlite"
   fi
   
+  echo "Using db backend $DB_TYPE."
   case $DB_TYPE in
     sqlite)
       sed -e "s#\$DB_PREFIX#$DB_PREFIX#" < /template/autoconfig_sqlite.php >> /var/www/html/owncloud/config/autoconfig.php
