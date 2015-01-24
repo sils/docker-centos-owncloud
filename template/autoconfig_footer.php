192ae78dc377c980ef3e4466712a8ad59b2946e7
cw1 <cw1@users.noreply.github.com>
2015-01-24 13:12:52 +0100

    created template for autoconfig.php footer

---
+++ template/autoconfig_footer.php
@@ -0,0 +1,13 @@
+  "appcodechecker" => false,
+  "apps_paths" => array (
+    0 => array (
+      "path"     => OC::$SERVERROOT."/apps",
+      "url"      => "/apps",
+      "writable" => false,
+    ),
+    1 => array (
+      "path"     => OC::$SERVERROOT."/extra_apps",
+      "url"      => "/extra_apps",
+      "writable" => true,
+    ),
+);
