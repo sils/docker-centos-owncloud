b140a085519d1ab9c5e8487618e40f8074d82d8e
cw1 <cw1@users.noreply.github.com>
2015-01-24 12:19:15 +0100

    created autoconfig mysql template

---
+++ template/autoconfig_mysql.php
@@ -0,0 +1,9 @@
+<?php
+$AUTOCONFIG = array(
+  "directory"     => "/data",
+  "dbtype"        => "mysql",
+  "dbname"        => "owncloud",
+  "dbuser"        => "$MYSQL_USER",
+  "dbpass"        => "$MYSQL_ENV_MYSQL_ROOT_PASSWORD",
+  "dbhost"        => "$MYSQL_HOST",
+  "dbtableprefix" => "$DB_PREFIX",
