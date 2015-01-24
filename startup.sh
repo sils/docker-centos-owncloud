a7a47c6e4f057861eb4c1de97d3c725c41b614c1
cw1 <cw1@users.noreply.github.com>
2015-01-24 12:16:00 +0100

    added autoconfig_sqlite.php template parsing

--- startup.sh
+++ startup.sh
@@ -13,6 +13,15 @@ else
   else
     OWNCLOUD_ALIAS="<IfModule mod_alias.c> Alias /$OWNCLOUD_URL_PATH /var/www/html/owncloud </IfModule>"
   sed -e "s#\$OWNCLOUD_ALIAS#$OWNCLOUD_ALIAS#" < /template/owncloud.conf >> /etc/httpd/conf.d/owncloud.conf
+  
+  # generating autoconfig.php db part through template
+  if [ -z "$DB_TYPE" ]; then
+    DB_TYPE=sqlite
+  fi
+  
+  case $DB_TYPE in
+    sqlite)
+      sed -e "s#\$DB_PREFIX#$DB_PREFIX#" < /template/autoconfig_sqlite.php >> /var/www/html/owncloud/config/autoconfig.php
 fi
 
 /usr/sbin/httpd -DFOREGROUND -k start
